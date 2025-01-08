01588efc65160a6e9397848ed607600a8b210f02
Sanketmahajan03 <sanketmahajan402@gmail.com>
2025-01-08 22:13:31 +0530

    Add project files.

---
+++ NZWalks-Learning/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks_Learning.Model.Domain;
+
+namespace NZWalks_Learning.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walks> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ NZWalks-Learning/Model/Domain/Walks.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks_Learning.Model.Domain
+{
+    public class Walks
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation properties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
